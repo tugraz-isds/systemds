b7911af92a0e9a8440cb60d6248ab58c5b80c5a7
Afan Secic <afansecic2@gmail.com>
2020-01-18 14:52:19 +0100

    Fixed reading the data and fixed learning and generating part so it is more generic

--- scripts/staging/hmm/HMM Training.py
+++ scripts/staging/hmm/HMM Training.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import random
-
+from itertools import combinations
 def add2dict(dictionary, key, value):
     if key not in dictionary:
         dictionary[key] = []
@@ -17,21 +17,66 @@ def list2probabilitydict(given_list):
     return probability_dict
 
 def sample_word(dictionary):
-    key = np.random.choice(list(dictionary.keys()), 1, p = list(dictionary.values()))[0]
-    return key
+    p0 = np.random.random()
+    cumulative = 0
+    for key, value in dictionary.items():
+        cumulative += value
+        if p0 < cumulative:
+            return key
 
-def generate(sentence, no_of_words):
+def generate_generic(sentence, no_of_words_to_generate, previous_words):
     sentence = sentence.split()
+    if len(sentence) < previous_words:
+        previous_words = len(sentence)
     if len(sentence) == 0:
-        word0 = sample_word(initial_word)        
-        sentence.append(word0)
-
+        sentence.append(sample_word(initial_word))
+        no_of_words_to_generate = no_of_words_to_generate - 1
     if len(sentence) == 1:
         word0 = sentence[0]
         if word0 in second_word.keys():
             word1 = sample_word(second_word[word0])
         else:
-            word1 = np.random.choice(list(initial_word.keys()), 1, p = list(initial_word.values()))[0]
+            word1 = np.random.choice(list(second_word[word0].keys()), 1, p = list(second_word[word0].values()))[0]
+        sentence.append(word1)
+        no_of_words_to_generate = no_of_words_to_generate - 1
+    
+    while no_of_words_to_generate > 0:
+        existing_keys = []
+        previous_words_temp = previous_words
+        found_keys = False
+        while previous_words_temp != 0:
+            words = list(combinations(sentence, previous_words_temp))
+            previous_words_temp = previous_words_temp - 1
+            existing_keys = list(set(words).intersection(transitions))
+            if(len(existing_keys) != 0):
+                found_keys = True
+                break
+        if found_keys:
+            existing_keys = np.array(existing_keys)
+            chosen_key = tuple(existing_keys[np.random.choice(len(existing_keys),1)][0])
+            word = np.random.choice(list(transitions[chosen_key].keys()), 1, p = list(transitions[chosen_key].values()))[0]
+            sentence.append(word)
+            no_of_words_to_generate = no_of_words_to_generate - 1
+        else:
+            chosen_key = np.random.choice(list(transitions.keys()), 1)[0]
+            word = np.random.choice(list(transitions[chosen_key].keys()), 1, p = list(transitions[chosen_key].values()))[0]
+            sentence.append(word)
+            no_of_words_to_generate = no_of_words_to_generate - 1
+        
+    print(' '.join(sentence))
+
+#Old function, not used
+def generate(sentence, no_of_words):
+    sentence = sentence.split()
+    if len(sentence) == 0:
+        word0 = sample_word(initial_word)        
+        sentence.append(word0)
+    if len(sentence) == 1:
+        word0 = sentence[0]
+        if word0 in second_word.keys():
+            word1 = sample_word(second_word[word0])
+        else:
+            word1 = np.random.choice(list(second_word[word0].keys()), 1, p = list(second_word[word0].values()))
         sentence.append(word1)
         no_of_words = no_of_words - 1
     
@@ -44,22 +89,24 @@ def generate(sentence, no_of_words):
             if word0 in second_word.keys():
                 word2_tmp1 = sample_word(second_word[word0])
                 dict1 = second_word[word2_tmp1]
-                word2_tmp1 = sample_word(dict1)
+                word2_tmp1 = np.random.choice(list(dict1), 1, p = list(dict1.values()))[0]
             else:
                 word2_tmp1 = np.random.choice(list(second_word.keys()), 1)
                 dict1 = second_word[word2_tmp1[0]]
-                word2_tmp1 = sample_word(dict1)
+                word2_tmp1 = np.random.choice(list(dict1), 1, p = list(dict1.values()))[0]
             if word1 in second_word.keys():
                 word2_tmp2 = sample_word(second_word[word1])
                 dict2 = second_word[word2_tmp2]
-                word2_tmp2 = sample_word(dict2)
+                word2_tmp2 = np.random.choice(list(dict2), 1, p = list(dict2.values()))[0]
             else:
                 word2_tmp2 = np.random.choice(list(second_word.keys()), 1)
                 dict2 = second_word[word2_tmp2[0]]
-                word2_tmp2 = sample_word(dict2)
+                word2_tmp2 = np.random.choice(list(dict2), 1, p = list(dict2.values()))[0]
             word2 = word2_tmp1 if dict1[word2_tmp1] > dict2[word2_tmp2] else word2_tmp2
         sentence.append(word2)
         no_of_words = no_of_words - 1
+
+    
     
     while no_of_words > 0:
         if (word0, word1, word2) in transitions.keys():
@@ -75,8 +122,8 @@ def generate(sentence, no_of_words):
                 if word2 in second_word.keys():
                     word3 = sample_word(second_word[word2])
                 else:
-                    word3_keys = np.random.choice(list(transitions.values()), 1)
-                    word3 = sample_word(word3_keys)
+                    key = np.random.choice(list(transitions.values()), 1)[0]
+                    word3 = np.random.choice(list(key.keys()), 1, p = list(key.values()))
 
         sentence.append(word3)
         word0 = word1
@@ -85,6 +132,32 @@ def generate(sentence, no_of_words):
         no_of_words = no_of_words - 1
     print(' '.join(sentence))
 
+def train_markov_model_generic(data, no_of_words):
+    for line in data:
+        line_length = len(line)
+        first_token = line[0]
+        initial_word[first_token] = initial_word.get(first_token, 0) + 1
+        for i in range(1,line_length-1):
+            for j in range(len(line[:i+1]) if len(line[:i+1]) < no_of_words + 1 else no_of_words + 1):
+                word_combinations = combinations(line[:i+1], j)
+                for combination in list(word_combinations):
+                    if len(combination) > 0:    
+                        if i == 1:
+                            add2dict(second_word, combination if len(combination) > 1 else combination[0], line[i+1])
+                        else:
+                            add2dict(transitions, combination if len(combination) > 1 else combination[0], line[i+1])
+
+    initial_word_total = sum(initial_word.values())
+    for key, value in initial_word.items():
+        initial_word[key] = value / initial_word_total
+        
+    for prev_word, next_word_list in second_word.items():
+        second_word[prev_word] = list2probabilitydict(next_word_list)
+        
+    for word_pair, next_word_list in transitions.items():
+        transitions[word_pair] = list2probabilitydict(next_word_list)
+
+#Old function, not used
 def train_markov_model(data):
     for line in data:
         line_length = len(line)
@@ -130,6 +203,7 @@ data = np.array([row[~pd.isnull(row)] for row in data])
 initial_word = {}
 second_word = {}
 transitions = {}
-train_markov_model(data)
-sentence = 'cocoa bahia'
-generate(sentence,2)
+#Second parameter is to determine how many previous words algorithm takes when learning
+train_markov_model_generic(data, 5)
+sentence = ''
+generate_generic(sentence,8,5)
--- scripts/staging/hmm/HMM.py
+++ scripts/staging/hmm/HMM.py
@@ -1,30 +1,43 @@
 from bs4 import BeautifulSoup,SoupStrainer
 import nltk
-nltk.download('wordnet')
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import string
 import numpy as np
 import csv
-f = open('reut2-000.sgm', 'r')
-data= f.read()
-lemmatizer = WordNetLemmatizer() 
-soup = BeautifulSoup(data,'html.parser')
-sentences = []
-text_matrix = []
-for item in soup.findAll('body'):
-    sentences = sent_tokenize(item.text)
-    for sentence in sentences:
-        text_matrix.append([token for token in word_tokenize(sentence) if token.lower() not in stopwords.words('english') and token not in string.punctuation])
-for i in range(len(text_matrix)):
-    for j in range(len(text_matrix[i])):
-       text_matrix[i][j] = lemmatizer.lemmatize(text_matrix[i][j].lower(), pos='v')
-length = max(map(len, text_matrix))
-text_matrix = [x + ['.'] for x in text_matrix]
-text_matrix=np.array([row + [None] * (length - len(row)) for row in text_matrix])
-for row in text_matrix:
-    with open('text_matrix.csv', 'a', newline='') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerow(row)
-    csvFile.close()
+import os
+
+def create_dataset(input_file_name, output_file_name):
+    f = open(input_file_name, 'r')
+    data= f.read()
+    lemmatizer = WordNetLemmatizer() 
+    soup = BeautifulSoup(data,'html.parser')
+    sentences = []
+    text_matrix = []
+    for item in soup.findAll('body'):
+        sentences = sent_tokenize(item.text)
+        for sentence in sentences:
+            text_matrix.append([token for token in word_tokenize(sentence) if token.lower() not in stopwords.words('english') and token not in string.punctuation])
+    for i in range(len(text_matrix)):
+        for j in range(len(text_matrix[i])):
+            text_matrix[i][j] = lemmatizer.lemmatize(text_matrix[i][j].lower(), pos='v')
+    length = max(map(len, text_matrix))
+    text_matrix=np.array([row + [None] * (length - len(row)) for row in text_matrix])
+    try:
+        with open(output_file_name) as csvFile:
+            os.remove(output_file_name)
+            csvFile = open(output_file_name, 'w')
+            for row in text_matrix:
+                writer = csv.writer(csvFile)
+                writer.writerow(row)
+        csvFile.close() 
+    except IOError:
+        with open(output_file_name, 'w') as csvFile:
+            for row in text_matrix:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(row)
+            csvFile.close() 
+
+
+create_dataset('reut2-000.sgm', 'text_matrix2.csv')
