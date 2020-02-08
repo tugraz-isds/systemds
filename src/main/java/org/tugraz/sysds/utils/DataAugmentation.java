169ca399e5247805708c759bee1c3346be974759
Enrique Barba <enriquebarba97@gmail.com>
2020-02-08 16:16:32 +0100

    - Added swap errors. - Added test for typos, missing values, and swapped values. - Added documentation. - Some cleaning.

--- src/main/java/org/tugraz/sysds/utils/DataAugmentation.java
+++ src/main/java/org/tugraz/sysds/utils/DataAugmentation.java
@@ -11,37 +11,48 @@ import org.tugraz.sysds.common.Types.ValueType;
 import org.tugraz.sysds.runtime.matrix.data.FrameBlock;
 
 public class DataAugmentation {
-	
-	// Refactoring, separate methods for each kind of error
-	
+		
 	/**
 	 * This function returns a new frame block with error introduced in the data:
-	 * Typos in string values, null values, outliers in numeric data and swapped elements 
+	 * Typos in string values, null values, outliers in numeric data and swapped elements.
 	 * 
 	 * @param input Original frame block
 	 * @param pTypo Probability of introducing a typo in a row
 	 * @param pMiss Probability of introducing missing values in a row
+	 * @param pDrop Probability of dropping a value inside a row
 	 * @param pOut Probability of introducing outliers in a row
 	 * @param pSwap Probability swapping two elements in a row
 	 * @return A new frameblock with corrupted elements
 	 * 
 	 */
-	public static FrameBlock dataCorruption(FrameBlock input, double pTypo, double pMiss, double pOut, double pSwap) {
-		List<Integer> numerics = new ArrayList<>();
-		List<Integer> strings = new ArrayList<>();
+	public static FrameBlock dataCorruption(FrameBlock input, double pTypo, double pMiss, double pDrop, double pOut, double pSwap) {
+		List<Integer> numerics = new ArrayList<Integer>();
+		List<Integer> strings = new ArrayList<Integer>();
+		List<Integer> swappable = new ArrayList<Integer>();
 
-		FrameBlock res = preprocessing(input, numerics, strings);
+		FrameBlock res = preprocessing(input, numerics, strings, swappable);
 		
-		res = typos(res, pTypo);
+		res = typos(res, strings, pTypo);
 		
-		res = miss(res, pMiss);
+		res = miss(res, pMiss, pDrop);
 		
-		res = outlier(res, pOut, 3);
+		res = outlier(res, numerics, pOut, 0.5, 3);
 		
 		return res;
 	}
 	
-	public static FrameBlock preprocessing(FrameBlock frame, List<Integer> numerics, List<Integer> strings) {
+	/**
+	 * This function returns a new frame block with a labels column added, and build the lists
+	 * with column index of the different types of data.
+	 * 
+	 * @param frame Original frame block
+	 * @param numerics Empty list to return the numeric positions
+	 * @param strings Empty list to return the string positions
+	 * @param swappable Empty list to return the swappable positions
+	 * @return A new frameblock with a labels column
+	 * 
+	 */
+	public static FrameBlock preprocessing(FrameBlock frame, List<Integer> numerics, List<Integer> strings, List<Integer> swappable) {
 		FrameBlock res = new FrameBlock(frame);
 		for(int i=0;i<res.getNumColumns();i++) {
 			if(res.getSchema()[i].isNumeric()) { 
@@ -49,22 +60,38 @@ public class DataAugmentation {
 			}else if(res.getSchema()[i].equals(ValueType.STRING)) {
 				strings.add(i);
 			}
+			if(i!=res.getNumColumns()-1 && res.getSchema()[i].equals(res.getSchema()[i+1])) {
+				swappable.add(i);
+			}
 		}
+		
 		
 		String[] labels = new String[res.getNumRows()];
 		Arrays.fill(labels, "");
 		res.appendColumn(labels);
 		
+		res.getColumnNames()[res.getNumColumns()-1] = "errorLabels";
+		
+		
 		return res;
 	}
 	
-	public static FrameBlock typos(FrameBlock frame, double pTypo) {
-		List<Integer> strings = new ArrayList<Integer>();
-		for(int i=0;i<frame.getNumColumns();i++) {
-			if(frame.getSchema()[i].equals(ValueType.STRING)) { 
-				strings.add(i);
-			}
+	/**
+	 * This function modifies the given, preprocessed frame block to add typos to the string values,
+	 * marking them with the label typos.
+	 * 
+	 * @param frame Original frame block
+	 * @param strings List with the columns of string type, generated during preprocessing
+	 * @param pTypo Probability of adding a typo to a row
+	 * @return A new frameblock with typos
+	 * 
+	 */
+	public static FrameBlock typos(FrameBlock frame, List<Integer> strings, double pTypo) {
+		
+		if(!frame.getColumnName(frame.getNumColumns()-1).equals("errorLabels")) {
+			throw new IllegalArgumentException("The FrameBlock passed has not been preprocessed.");
 		}
+		
 		if(strings.isEmpty()) return frame;
 		
 		Random rand = new Random();
@@ -73,8 +100,10 @@ public class DataAugmentation {
 			String s = (String) frame.get(r, c);
 			if(s.length()!=1 && rand.nextDouble()<=pTypo) {
 				int i = rand.nextInt(s.length());
-				if(i!=s.length()-1 && rand.nextDouble()<=0.5) s = swap(s, i, i+1);
-				else s = swap(s, i-1, i);
+				if(i==s.length()-1) s = swapchr(s, i-1, i);
+				else if(i==0) s = swapchr(s, i, i+1);
+				else if(rand.nextDouble()<=0.5) s = swapchr(s, i, i+1);
+				else s = swapchr(s, i-1, i);
 				frame.set(r, c, s);
 				
 				String label = (String) frame.get(r, frame.getNumColumns()-1);
@@ -85,28 +114,62 @@ public class DataAugmentation {
 		return frame;
 	}
 	
-	public static FrameBlock miss(FrameBlock frame, double pMiss) {
+	
+	/**
+	 * This function modifies the given, preprocessed frame block to add missing values to some of the rows,
+	 * marking them with the label missing.
+	 * 
+	 * @param frame Original frame block
+	 * @param pMiss Probability of adding missing values to a row
+	 * @param pTypo Probability of dropping a value from a row previously selected by pTypo
+	 * @return A new frameblock with missing values
+	 * 
+	 */
+	public static FrameBlock miss(FrameBlock frame, double pMiss, double pDrop) {
+		if(!frame.getColumnName(frame.getNumColumns()-1).equals("errorLabels")) {
+			throw new IllegalArgumentException("The FrameBlock passed has not been preprocessed.");
+		}
 		Random rand = new Random();
 		for(int r=0;r<frame.getNumRows();r++) {
 			if(rand.nextDouble()<=pMiss) {
-				int c = rand.nextInt(frame.getSchema().length);
-				frame.set(r, c, null);
-				
-				String label = (String) frame.get(r, frame.getNumColumns()-1);
-				if(label.equals("")) frame.set(r, frame.getNumColumns()-1, "missing");
-				else frame.set(r, frame.getNumColumns()-1, label + ",missing");
+				int dropped = 0;
+				for(int c=0;c<frame.getNumColumns()-1;c++) {
+					if((frame.get(r, c)!=null && !frame.get(r, c).equals(0)) && rand.nextDouble()<=pDrop) {
+						frame.set(r, c, null);
+						dropped++;
+					}
+				}
+					
+				if(dropped>0) {
+					String label = (String) frame.get(r, frame.getNumColumns()-1);
+					if(label.equals("")) frame.set(r, frame.getNumColumns()-1, "missing");
+					else frame.set(r, frame.getNumColumns()-1, label + ",missing");	
+				}
 			}
 		}
 		return frame;
 	}
 	
-	public static FrameBlock outlier(FrameBlock frame, double pOut, int times) {
-		List<Integer> numerics = new ArrayList<Integer>();
-		for(int i=0;i<frame.getNumColumns();i++) {
-			if(frame.getSchema()[i].isNumeric()) { 
-				numerics.add(i);
-			}
+	/**
+	 * This function modifies the given, preprocessed frame block to add outliers to some
+	 * of the numeric data of the frame, adding or  several times the standard deviation,
+	 * and marking them with the label outlier.
+	 * 
+	 * @param frame Original frame block
+	 * @param numerics List with the columns of numeric type, generated during preprocessing
+	 * @param pOut Probability of introducing an outlier in a row
+	 * @param pPos Probability of using positive deviation
+	 * @param times Times the standard deviation is added
+	 * @return A new frameblock with outliers
+	 * 
+	 */
+	public static FrameBlock outlier(FrameBlock frame, List<Integer> numerics, double pOut, double pPos, int times) {
+		
+		if(!frame.getColumnName(frame.getNumColumns()-1).equals("errorLabels")) {
+			throw new IllegalArgumentException("The FrameBlock passed has not been preprocessed.");
 		}
+		
+		if(numerics.isEmpty()) return frame;
 		
 		Map<Integer, Double> stds = new HashMap<Integer, Double>();
 		Random rand = new Random();
@@ -207,19 +270,23 @@ public class DataAugmentation {
 			Double std = stds.get(c);
 			if(frame.getSchema()[c].equals(ValueType.INT32)) {
 				Integer val = (Integer) frame.get(r, c);
-				val += (int) Math.round(times*std);
+				if(rand.nextDouble()<=pPos) val += (int) Math.round(times*std);
+				else val -= (int) Math.round(times*std);
 				frame.set(r, c, val);
 			} else if(frame.getSchema()[c].equals(ValueType.INT64)) {
 				Long val = (Long) frame.get(r, c);
-				val += Math.round(times*std);
+				if(rand.nextDouble()<=pPos) val += Math.round(times*std);
+				else val -= Math.round(times*std);
 				frame.set(r, c, val);
 			} else if(frame.getSchema()[c].equals(ValueType.FP32)) {
 				Float val = (Float) frame.get(r, c);
-				val += (float) (times*std);
+				if(rand.nextDouble()<=pPos) val += (float) (times*std);
+				else val -= (float) (times*std);
 				frame.set(r, c, val);
 			} else if(frame.getSchema()[c].equals(ValueType.FP64)) {
 				Double val = (Double) frame.get(r, c);
-				val += times*std;
+				if(rand.nextDouble()<=pPos) val += times*std;
+				else val -= times*std;
 				frame.set(r, c, val);
 			}
 			
@@ -230,9 +297,39 @@ public class DataAugmentation {
 		
 		return frame;
 	}
+	/**
+	 * This function modifies the given, preprocessed frame block to add swapped fields of the same ValueType
+	 * that are consecutive, marking them with the label swap.
+	 * 
+	 * @param frame Original frame block
+	 * @param swappable List with the columns that are swappable, generated during preprocessing
+	 * @param pSwap Probability of swapping two fields in a row
+	 * @return A new frameblock with swapped elements
+	 * 
+	 */
+	public static FrameBlock swap(FrameBlock frame, List<Integer> swappable, double pSwap) {
+		if(!frame.getColumnName(frame.getNumColumns()-1).equals("errorLabels")) {
+			throw new IllegalArgumentException("The FrameBlock passed has not been preprocessed.");
+		}
+		
+		Random rand = new Random();
+		for(int r=0;r<frame.getNumRows();r++) {
+			if(rand.nextDouble()<=pSwap) {
+				int i = swappable.get(rand.nextInt(swappable.size()));
+				Object tmp = frame.get(r, i);
+				frame.set(r, i, frame.get(r, i+1));
+				frame.set(r, i+1, tmp);
+				
+				String label = (String) frame.get(r, frame.getNumColumns()-1);
+				if(label.equals("")) frame.set(r, frame.getNumColumns()-1, "swap");
+				else frame.set(r, frame.getNumColumns()-1, label + ",swap");
+			}
+		}
+		
+		return frame;
+	}
 	
-	
-	private static String swap(String str, int i, int j) 
+	private static String swapchr(String str, int i, int j) 
     { 
         if (j == str.length() - 1) 
             return str.substring(0, i) + str.charAt(j) 
--- src/test/java/org/tugraz/sysds/test/applications/DataCorruptionTest.java
+++ src/test/java/org/tugraz/sysds/test/applications/DataCorruptionTest.java
@@ -25,6 +25,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tugraz.sysds.common.Types.ValueType;
 import org.tugraz.sysds.runtime.matrix.data.FrameBlock;
@@ -32,6 +34,36 @@ import org.tugraz.sysds.utils.DataAugmentation;
 
 public class DataCorruptionTest{
 
+	private static FrameBlock ori;
+	private FrameBlock changed;
+	private List<Integer> numerics;
+	private List<Integer> stringpos;
+	private List<Integer> swappable;
+	
+	@BeforeClass
+	public static void init() {
+		ValueType[] schema = new ValueType[] {ValueType.STRING, ValueType.INT32};
+		ori = new FrameBlock(schema);
+		String[] strings = new String[] { 
+				  "Toyota", "Mercedes", "BMW", "Volkswagen", "Skoda" };
+		Random rand = new Random();
+		
+		for(int i = 0; i<1000; i++) {
+			Object[] row = new Object[2];
+			row[0] = strings[rand.nextInt(strings.length)];
+			row[1] = rand.nextInt(1000);
+			
+			ori.appendRow(row);
+		}
+	}
+	
+	@Before
+	public void testInit() {
+		numerics = new ArrayList<Integer>();
+		stringpos = new ArrayList<Integer>();
+		swappable = new ArrayList<Integer>();
+		changed = DataAugmentation.preprocessing(ori, numerics, stringpos, swappable);
+	}
 	// Split into multiple test
 	
 //	@Test
@@ -70,39 +102,106 @@ public class DataCorruptionTest{
 //		}
 //	}
 	
+	@Test
+	public void testTypos() {
+		changed = DataAugmentation.typos(changed, stringpos, 0.2);
+		
+		double numch = 0.;
+		for(int i=0;i<changed.getNumRows();i++) {
+			String label = (String) changed.get(i, changed.getNumColumns()-1);
+			if(label.equals("typo")) {
+				numch++;
+				String val = (String) ori.get(i, 0);
+				String valch = (String) changed.get(i, 0);
+				assertNotEquals("Row "+i+" was marked with typos, but it has not been changed.", val, valch);
+			}
+		}
+		System.out.println("Test typos: number of changed rows: " + numch);
+		assertEquals("The number of changed rows is not approx. 20%", 0.2, numch/changed.getNumRows(), 0.05);
+		
+	}
 	
+	@Test
+	public void testMiss() {
+		changed = DataAugmentation.miss(changed, 0.2, 0.7);
+		
+		double numch = 0.;
+		for(int i=0;i<changed.getNumRows();i++) {
+			String label = (String) changed.get(i, changed.getNumColumns()-1);
+			if(label.equals("missing")) {
+				int dropped = 0;
+				for(int c=0;c<ori.getNumColumns();c++) {
+					if((ori.get(i, c)!=null && !ori.get(i, c).equals(0)) && (changed.get(i, c)==null || changed.get(i, c).equals(0))) dropped++;
+				}
+				numch++;
+				assertTrue("Row "+i+" was marked with missing values, but it has not been changed.", dropped>0);
+			}
+		}
+		System.out.println("Test missing: number of changed rows: " + numch);
+		assertEquals("The number of changed rows is not approx. 20%", 0.2, numch/changed.getNumRows(), 0.05);
+	}
 	
 	@Test
 	public void testOutliers() {
-		ValueType[] schema = new ValueType[] {ValueType.STRING, ValueType.INT32};
-		FrameBlock ori = new FrameBlock(schema);
-		String[] strings = new String[] { 
-				  "Toyota", "Mercedes", "BMW", "Volkswagen", "Skoda" };
-		Random rand = new Random();
+		changed = DataAugmentation.outlier(changed, numerics, 0.2, 0.5, 3);
 		
-		for(int i = 0; i<1000; i++) {
-			Object[] row = new Object[2];
-			row[0] = strings[rand.nextInt(strings.length)];
-			row[1] = rand.nextInt(1000);
-			
-			ori.appendRow(row);
-		}
-		
-		List<Integer> numerics = new ArrayList<Integer>();
-		List<Integer> stringpos = new ArrayList<Integer>();
-		FrameBlock changed = DataAugmentation.preprocessing(ori, numerics, stringpos);
-		changed = DataAugmentation.outlier(changed, 0.2, 3);
-		
-		int numch = 0;
+		double numch = 0.;
 		for(int i=0;i<changed.getNumRows();i++) {
 			String label = (String) changed.get(i, changed.getNumColumns()-1);
 			if(label.equals("outlier")) {
 				numch++;
 				Integer val = (Integer) ori.get(i, 1);
 				Integer valch = (Integer) changed.get(i, 1);
-				assertTrue(val!=valch); 
+				assertNotEquals("Row "+i+" was marked with outliers, but it has not been changed.", val, valch);
 			}
 		}
-		System.out.println("Number of changed rows: " + numch);
+		System.out.println("Test outliers: number of changed rows: " + numch);
+		assertEquals("The number of changed rows is not approx. 20%", 0.2, numch/changed.getNumRows(), 0.05);
+	}
+	
+	@Test
+	public void testSwap() {
+		ValueType[] schema = new ValueType[] {ValueType.INT32, ValueType.INT32, ValueType.INT32};
+		FrameBlock ori = new FrameBlock(schema);
+		
+		Random rand = new Random();
+		
+		for(int i = 0; i<1000; i++) {
+			Object[] row = new Object[3];
+			row[0] = rand.nextInt(1000);
+			row[1] = rand.nextInt(1000);
+			row[2] = rand.nextInt(1000);
+			
+			ori.appendRow(row);
+		}
+		
+		List<Integer> numerics = new ArrayList<Integer>();
+		List<Integer> strings = new ArrayList<Integer>();
+		List<Integer> swappable = new ArrayList<Integer>();
+		
+		FrameBlock changed = DataAugmentation.preprocessing(ori, numerics, strings, swappable);
+		
+		changed = DataAugmentation.swap(changed, swappable, 0.2);
+		
+		double numch = 0.;
+		for(int i=0;i<changed.getNumRows();i++) {
+			String label = (String) changed.get(i, changed.getNumColumns()-1);
+			if(label.equals("swap")) {
+				numch++;
+				boolean checkSwap = false;
+				for(int c:swappable) {
+					Integer val0 = (Integer) ori.get(i, c);
+					Integer valch0 = (Integer) changed.get(i, c);
+					Integer val1 = (Integer) ori.get(i, c+1);
+					Integer valch1 = (Integer) changed.get(i, c+1);
+					checkSwap = checkSwap || (val0.equals(valch1) && val1.equals(valch0));
+					if(checkSwap) break;
+				}
+				assertTrue("Row "+i+" was marked with outliers, but it has not been changed.",checkSwap);
+			}
+		}
+		System.out.println("Test swap: number of changed rows: " + numch);
+		assertEquals("The number of changed rows is not approx. 20%", 0.2, numch/changed.getNumRows(), 0.05);
+		
 	}
 }
