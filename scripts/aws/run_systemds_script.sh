ecc9732856cbe3f8c2ec342a126f3c01444277d4
Florijan Klezin <florijan.klezin@student.tugraz.at>
2020-02-25 18:12:48 +0100

    Formatted README.md Added licence headers. Removed Zeppelin application from create cluster command. Cleared up config.

--- scripts/aws/run_systemds_script.sh
+++ scripts/aws/run_systemds_script.sh
@@ -1,6 +1,34 @@
-#! /bin/bash
+#!/bin/bash
+#-------------------------------------------------------------
+#  Modifications Copyright 2019 Graz University of Technology
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+# under the License.
+#-------------------------------------------------------------
+
 # $1 Dml script name.
 # $2 Args
+
+# If number of arguments are not equal to 1.
+if [[ ($# -ne 2 && $# -ne 1) ]] ; then
+      echo "Usage: "$0" <DML script path> (<Arguments>)"
+      exit 2
+fi
+
 
 source systemds_cluster.config
 
--- scripts/aws/spinup_systemds_cluster.sh
+++ scripts/aws/spinup_systemds_cluster.sh
@@ -1,4 +1,24 @@
-#! /bin/bash
+#!/bin/bash
+#-------------------------------------------------------------
+#  Modifications Copyright 2019 Graz University of Technology
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+# under the License.
+#-------------------------------------------------------------
 
 source systemds_cluster.config
 
@@ -41,7 +61,7 @@ fi
 
 #Create the cluster
 CLUSTER_INFO=$(aws emr create-cluster \
- --applications Name=Ganglia Name=Spark Name=Zeppelin \
+ --applications Name=Ganglia Name=Spark \
  --ec2-attributes '{"KeyName":"'${KEYPAIR_NAME}'",
   "InstanceProfile":"EMR_EC2_DefaultRole"}'\
  --service-role EMR_DefaultRole \
