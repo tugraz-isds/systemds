64f452a4d8fb948e59346977c26397dae2b1923e
Mark Dokter <mark@dokter.cc>
2020-03-17 01:52:39 +0100

    a simple (for the user) bash run script: runSystemDS.sh

--- src/main/standalone/runSystemDS.sh
+++ src/main/standalone/runSystemDS.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #-------------------------------------------------------------
 #
 # Modifications Copyright 2020 Graz University of Technology
@@ -31,8 +31,11 @@
 # Make configuration changes here:
 ##############################################################
 
-# run locally by default
+# set to 1 to run spark-submit instead of local java
 DISTRIBUTED=0
+
+# set to 1 to disable setup output of this script
+QUIET=0
 
 # specify paramteters  to java when running locally here
 SYSTEMDS_STANDALONE_OPTS="\
@@ -54,40 +57,34 @@ SYSTEMDS_DISTRIBUTED_OPTS="\
 # SystemDS based on the settings above.
 ##############################################################
 
-# changes the separator in a string (eg to convert from / directory separator to \
-
-change_delimiter()
-{
-  output=$( "IFS=$2 echo $1" )
-  echo "$output"
-}
-
-change_delimiter $(pwd) ":"
-exit 0
 
 # error help print
 printUsageExit()
 {
 cat << EOF
-Usage: $0 <dml-filename> [arguments] [-help] [-dist]
-    arguments - The arguments specified after the DML script are passed to SystemDS.
-                Specify parameters that need to go to java/spark-submit by editing this
-                run script.
-    -help     - Print this usage message and exit
-    -dist     - Run distributed (invokes spark-submit). Default is to run locally (invokes java)
+Usage: $0 [SystemDS.jar] <dml-filename> [arguments] [-help]
+
+    SystemDS.jar - Specify a custom SystemDS.jar file (this will be prepended to the classpath
+                   or fed to spark-submit
+    dml-filename - The script file to run
+    arguments    - The arguments specified after the DML script are passed to SystemDS.
+                   Specify parameters that need to go to java/spark-submit by editing this
+                   run script.
+    -help        - Print this usage message and exit
 
 Set custom launch configuration by editing SYSTEMDS_STANDALONE_OPTS and/or SYSTEMDS_DISTRIBUTED_OPTS
 EOF
   exit 1
 }
 
-# option parsing
-options=$(getopt -l "help,dist" -a -o "hd" -- "$@")
-while options; do
+# print an error if no dml file is supplied
+if [ -z "$1" ] ; then
+    echo "Wrong Usage.";
+    printUsageExit;
+fi
+
+while getopts "h:" options; do
   case $options in
-    d )
-      DISTRIBUTED=1
-      ;;
     h ) echo Warning: Help requested. Will exit after usage message;
         printUsageExit
         ;;
@@ -96,49 +93,105 @@ while options; do
         ;;
     * ) echo Error: Unexpected error while processing options;
   esac
-  shift
 done
 
-# print an error if no dml file is supplied
-if [ -z $1 ] ; then
-    echo "Wrong Usage.";
-    printUsageExit;
-fi
+win_delim()
+{
+  declare -n ret=$1
+  OLDIFS=$IFS
+  IFS="/"
+  new_path=""
+  for i in ${ret}; do
+    new_path="${new_path}$i\\"
+  done
+  IFS=$OLDIFS
+  ret=${new_path:0:-1}
+}
 
-# Peel off first argument so that $@ contains arguments to DML script
-SCRIPT_FILE=$1
-shift
+status_output()
+{
+  if [ $QUIET == 0 ]; then
+    echo -e "$1"
+  fi
+}
 
-FIND_JAR_RESULT
-SYSTEMDS_JAR_FILE=
-find -iname systemds-?.?.?.jar
-
-# detect operating system to set correct directory separator
-if [ "$OSTYPE" == "win32" ] ||  [ "$OSTYPE" == "msys" ] ; then
-  DIR_SEP=\\
+# Peel off first and/or second argument so that $@ contains arguments to DML script
+if  grep -q "$1" <<< "jar"; then
+  SYSTEMDS_JAR_FILE=$1
+  shift
+  SCRIPT_FILE=$2
+  shift
 else
-  DIR_SEP=/
+  SCRIPT_FILE=$1
+  shift
 fi
 
-# set java class path
-CLASSPATH=".${DIR_SEP}lib${DIR_SEP}*"
+
+if [ -z "$SYSTEMDS_ROOT" ] ; then
+  SYSTEMDS_ROOT=.
+else
+  echo "Using existing SystemDS at ${SYSTEMDS_ROOT}"
+fi;
+
+
+# find me a SystemDS jar file to run
+if [ -z "${SYSTEMDS_JAR_FILE}" ];then
+  SYSTEMDS_JAR_FILE=$(find $SYSTEMDS_ROOT -iname "systemds.jar" | tail -n 1)
+  if [ -z "${SYSTEMDS_JAR_FILE}" ];then
+    SYSTEMDS_JAR_FILE=$(find $SYSTEMDS_ROOT -iname "systemds-?.?.?.jar" | tail -n 1)
+  fi
+fi
 
 # check if log4j config file exists, otherwise unset
 # to run with a non fatal complaint by SystemDS
-LOG4JPROP="conf${DIR_SEP}log4j.properties"
-if [ ! -f $LOG4JPROP ]; then
+#LOG4JPROP="conf${DIR_SEP}log4j.properties"
+#if [ ! -f "$LOG4JPROP" ]; then
+LOG4JPROP=$(find $SYSTEMDS_ROOT -iname "log4j*properties" | tail -n 1)
+if [ -z "${LOG4JPROP}" ]; then
   LOG4JPROP=""
 fi
 
 # same as above: set config file param if the
 # file exists
-CONFIG_FILE="conf${DIR_SEP}SystemDS-config.xml"
-if [ ! -f $CONFIG_FILE ]; then
+#CONFIG_FILE="conf${DIR_SEP}SystemDS-config.xml"
+#if [ ! -f $CONFIG_FILE ]; then
+CONFIG_FILE=$(find $SYSTEMDS_ROOT -iname "SystemDS*config*.xml" | tail -n 1)
+if [ -z "${CONFIG_FILE}" ]; then
   CONFIG_FILE=""
+else
+  CONFIG_FILE="--config ${CONFIG_FILE}"
 fi
 
+# detect operating system to set correct directory separator
+if [ "$OSTYPE" == "win32" ] ||  [ "$OSTYPE" == "msys" ] ; then
+  DIR_SEP=\\
+  CLASSPATH_SEP=;
+  # fix separator in find output on windows
+  win_delim SYSTEMDS_JAR_FILE
+  if [ -n "$LOG4JPROP" ]; then win_delim LOG4JPROP; fi
+  if [ -n "$CONFIG_FILE" ]; then win_delim CONFIG_FILE;  fi
+else
+  DIR_SEP=/
+  CLASSPATH_SEP=:
+fi
+
+# set java class path
+CLASSPATH="${SYSTEMDS_JAR_FILE}${CLASSPATH_SEP}\
+          ${SYSTEMDS_ROOT}${DIR_SEP}lib${DIR_SEP}*${CLASSPATH_SEP}\
+          ${SYSTEMDS_ROOT}${DIR_SEP}target${DIR_SEP}lib${DIR_SEP}*"
+# trim whitespace (introduced by the line breaks above)
+CLASSPATH=$(echo -e "$CLASSPATH" | tr -d '[:space:]')
+
+status_output "###############################################################################"
+status_output "#  SYSTEMDS_ROOT= ${SYSTEMDS_ROOT}"
+status_output "#  SYSTEMDS_JAR_FILE= ${SYSTEMDS_JAR_FILE}"
+status_output "#  CONFIG_FILE= ${CONFIG_FILE}"
+status_output "#  LOG4JPROP= ${LOG4JPROP}"
+
 #build the command to run
-if [ -z $DISTRIBUTED ]; then
+if [ ${DISTRIBUTED} == 0 ]; then
+  status_output "#\n#  Runnign locally with opts: $*"
+  status_output "###############################################################################"
   CMD=" \
   java ${SYSTEMDS_STANDALONE_OPTS} \
   -cp ${CLASSPATH} \
@@ -146,11 +199,13 @@ if [ -z $DISTRIBUTED ]; then
   org.tugraz.sysds.api.DMLScript \
   -f ${SCRIPT_FILE} \
   -exec singlenode \
-  -config $CONFIG_FILE \
+  $CONFIG_FILE \
   $*"
 else
-  export SPARK_MAJOR_VERSION=2
+  status_output "#\n#  Running distributed with opts: $*"
+  status_output "###############################################################################"
 
+  export SPARK_MAJOR_VERSION=2
   CMD=" \
   spark-submit ${SYSTEMDS_DISTRIBUTED_OPTS} \
   $SYSTEMDS_JAR_FILE \
